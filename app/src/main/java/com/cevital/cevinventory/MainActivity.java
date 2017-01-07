5151a9537b863e2a4986dd47ecd448ae19681c45
Athmane GHIDOUCHE (Analyste) <athmane.ghidouche@Cevital.com>
2017-01-07 20:58:29 +0100

    Initial commit

---
+++ app/src/main/java/com/cevital/cevinventory/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cevital.cevinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView text = new TextView(this);
+        text.setText("Bonjour, vous me devez 1 000 000€.");
+
+        setContentView(text);
+    }
+}
